ce71025f5be3a3a2876bff37ba75ce91c5265e19
Kidhar Bachan <kidhar.bachan@multichoice.co.za>
2020-04-20 21:21:49 +0200

    Priority queue working

--- astar.py
+++ astar.py
@@ -1,5 +1,7 @@
 #!/Users/kidharb/anaconda3/bin/python3
 
+# Pseudocode at https://en.wikipedia.org/wiki/A*_search_algorithm
+
 import gui
 from tkinter import *
 from queue import PriorityQueue
@@ -11,65 +13,70 @@ class Node():
         # reachable tracks whether we can occupy the block or not.
         # we cannot occupy the borders ar any obstacles in the gridworld.
         self.reachable = reachable
-
-        self.f = 0
-        self.g = 0
+        if parent == position:
+            self.gScore = 0
+        else:
+            self.gScore = 99999
+        self.fScore = 99999
+        
         # heuristic function is the manhattan distance
         self.h = abs(goal[1] - self.position[1]) + abs(goal[0] - self.position[0])
 
+
 def astar(app, grid, start, goal):
-    # Retrieve x nd y co-ordinates from starting point
-    x = start[0]
-    y = start[1]
 
     openSet = PriorityQueue()
-    openSet.put((grid[x][y].h,start))
-
-    cameFrom = []
-    gScore = {start:0}
-
-    fScore = {start:grid[x][y].h}
-    #print(fScore)
+    # Put the first node on the queue. Use h value as priority
+    openSet.put((grid[start[0]][start[1]].h,(start[0],start[1])))
     
-    #print(openSet.get()[1])
-    #while not openSet.empty():
-    # extract the tuple with the lowest f-score
-    current = openSet.get()[1]
-    if current == goal:
-        print("Goal reached - terminating")
+    cameFrom = {}
+
+    while not openSet.empty():
+        #print("Queue get",openSet.queue, openSet.empty)
+        # extract the tuple with the lowest f-score
+        temp =  openSet.get()[1]
+        current = grid[temp[0]][temp[1]]
+        if current.position == goal:
+            print("Goal reached - terminating")
+            return
+        for neighbour in [(grid[current.position[0]+1][current.position[1]]), (grid[current.position[0]][current.position[1]+1]), (grid[current.position[0]-1][current.position[1]]), (grid[current.position[0]][current.position[1]-1])]:
+            print(neighbour.position)
+            if (neighbour.reachable):
+                tentative_gScore = current.gScore + 1 # Neighbour so distance from previous node = 1
+                #print(gScore[current])
+                if tentative_gScore < neighbour.gScore:
+                    cameFrom[neighbour] = current
+                    neighbour.gScore = tentative_gScore
+                    neighbour.fScore = neighbour.gScore + neighbour.h
+                    #if neighbour in openSet.queue:
+                    #    print("Skip adding neighbour")#do nothing
+                    #else:
+                    openSet.put((neighbour.h, neighbour.position))
+                    #print("Current Queue",openSet.queue)
         
-
-
-    #while len(openSet) > 0:
-    #    current = openSet[0]
-    #    if current == goalNode:
-    #        return #reconstruct path
-
-    #    closedSet.append(current)
-    #    print(closedSet)
-    # Update the gui
-    #app.update_idletasks()
-    #app.update()
+    print("Failure : No route to goal from start")
+    return
 
 def gen_gridWorld(rows, cols, start, goal, obstacles):
     nodes = [[0] * rows for i in range(cols)]
     print("Parent node : x,y position : heuristic distance : reachable")
     for i in range(rows):
         for j in range(cols):
-            nodes[i][j] = Node((0,0),(i,j),goal,1)
             if ((i,j) == start):
                 nodes[i][j] = Node(start,(i,j),goal,1)
-            if i == 0:
+            elif i == 0:
                 nodes[i][j] = Node((0,0),(i,j),goal,0)
-            if i == (rows - 1):
+            elif i == (rows - 1):
                 nodes[i][j] = Node((0,0),(i,j),goal,0)
-            if j == 0:
+            elif j == 0:
                 nodes[i][j] = Node((0,0),(i,j),goal,0)
-            if j == (cols - 1):
+            elif j == (cols - 1):
                 nodes[i][j] = Node((0,0),(i,j),goal,0)
-            if (i,j) in obstacles:
+            elif (i,j) in obstacles:
                 nodes[i][j] = Node((0,0),(i,j),goal,0)
-            print(nodes[i][j].parent,nodes[i][j].position,nodes[i][j].h,nodes[i][j].reachable)
+            else:
+                nodes[i][j] = Node((0,0),(i,j),goal,1)    
+            print(nodes[i][j].position, nodes[i][j].h)
     return nodes
 
 if __name__ == "__main__" :
